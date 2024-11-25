3a98da350747bf8d3bff23d9d3fb24d159ac6ea1
Thomas Deinhamer <184284+thasmo@users.noreply.github.com>
2024-11-25 11:14:09 +0100

    ci: add semantic release workflow (#29)

--- knip.config.js
+++ knip.config.js
@@ -4,6 +4,10 @@ export default {
 		'commitlint-config-.*',
 		'@somehow-digital/commitlint-plugin-spellcheck',
 		'@commitlint/config-conventional',
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/github',
+		'@semantic-release/npm',
+		'@semantic-release/release-notes-generator',
 	],
 	project: ['configuration/**'],
 };
