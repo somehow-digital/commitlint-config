158ea3c7ae9868ad2ee81d1499f5647e17e1b686
Thomas Deinhamer <184284+thasmo@users.noreply.github.com>
2025-02-11 14:42:55 +0100

    ci: use shared semantic-release configuration (#74)

--- knip.config.js
+++ knip.config.js
@@ -4,10 +4,6 @@ export default {
 		'commitlint-config-.*',
 		'@somehow-digital/commitlint-plugin-spellcheck',
 		'@commitlint/config-conventional',
-		'@semantic-release/commit-analyzer',
-		'@semantic-release/github',
-		'@semantic-release/npm',
-		'@semantic-release/release-notes-generator',
 	],
 	project: ['configuration/**'],
 };
