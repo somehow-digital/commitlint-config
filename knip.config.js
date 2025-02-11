9127c4fb91915562fc1229a0cd948dc1931e8d2d
Thomas Deinhamer <code@thasmo.dev>
2025-02-11 15:13:25 +0100

    Install shared semantic-release configuration

--- knip.config.js
+++ knip.config.js
@@ -2,8 +2,9 @@ export default {
 	entry: ['configuration/*.js'],
 	ignoreDependencies: [
 		'commitlint-config-.*',
-		'@somehow-digital/commitlint-plugin-spellcheck',
 		'@commitlint/config-conventional',
+		'@somehow-digital/commitlint-plugin-spellcheck',
+		'@somehow-digital/semantic-release-config',
 	],
 	project: ['configuration/**'],
 };
